1208164150c1293f3628f56a33936be306505620
Alberto Di Maio <albertodimaio05@gmail.com>
2025-03-28 00:01:54 +0100

    v5

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -186,7 +186,7 @@ const Homepage = () => {
           </div>
           <div className="bg-[#e3ffe0] md:h-[164px] bg-opacity-10 mt-6 p-5 border border-[#658257] rounded-lg">
             <h2 className="font-semibold text-[17px] text-[#83d656]">Stay tuned</h2>
-            <p className="text-[#658257] mt-1 text-sm">Don’t subscribe if you’re fine with mediocrity while I crush it.</p>
+            <p className="text-[#658257] mt-1 text-sm">If you’re happy writing JavaScript in Notepad, don’t subscribe.</p>
             <div className="mt-6 md:flex gap-4 w-full">
               <div className="flex gap-4">
                 {res.message != null ? (
